95ef73c5c0d810714598eaf304b8fc2734e4242a
AboodKass <abood.kassoumeh@gmail.com>
2021-02-21 03:33:31 +0300

    python: printing the right route for nv=1.

--- python/TSP.py
+++ python/TSP.py
@@ -3,9 +3,11 @@
 # TSP: if there is just one vehicle (TSP.py TR 10 1 1 1 1 h 2)
 ### python3 TSP.py pHC_MTSP_10_1.json TR 10 1 1 1 1 h 1 s 2,3,4,5,6,7,8,9,10 ###
 
-import DATA3, sys
-from gurobipy import *
 from time import clock
+
+from gurobipy import *
+
+import DATA3
 from DATA3 import Timer
 
 # BURASI DA AYNI HUB LARI VERDIGIMIZDE KULLANABILDIGIMIZ HALE GETIRMEMIZ GEREKLI
@@ -20,7 +22,7 @@ V = range(nv)  # one vehicle type per hub
 N = DATA3.N
 dictionary = dict(zip([*range(len(N))], N))
 N = [*range(len(N))]
-NH = [*range(len(N))]   # non-hub nodes
+NH = [*range(len(N))]  # non-hub nodes
 HH = H
 H = []
 for hh in HH:
@@ -82,7 +84,6 @@ for i in N:
         for v in V:
             x[i][j].append(m.addVar(lb=0, vtype=GRB.BINARY, name="x"))
 
-
 collect = m.addVars(H, N, NH, V, lb=0, vtype=GRB.BINARY, name="collect")
 
 distribute = m.addVars(NH, N, H, V, lb=0, vtype=GRB.BINARY, name="distribute")
@@ -98,99 +99,46 @@ for indxs in range(len(N)):
     h[indxs].BranchPriority = 100
 # Create constraints
 # Degree constraint
-#*kisit101(v,i)..sum(j$(ord(j)<>ord(i)),x(i,j,v))-sum(j$(ord(j)<>ord(i)),x(j,i,v))=e=0;
-#kisit2(i,v)..sum(j$(ord(j)<>ord(i)),x(i,j,v))-sum(j$(ord(j)<>ord(i)),x(j,i,v))=e=0;
-#kisit2(i,v)..sum(j$(ord(j)<>ord(i)),x(i,j,v))-sum(j$(ord(j)<>ord(i)),x(j,i,v))=e=0;
+
+# kisit2(i,v)..sum(j$(ord(j)<>ord(i)),x(i,j,v))-sum(j$(ord(j)<>ord(i)),x(j,i,v))=e=0;
 for v in V:
     for i in N:
         m.addConstr(
             quicksum(x[i][j][v] for j in N if j != i) - quicksum(x[j][i][v] for j in N if j != i) == 0, name="ctr1")
 
-#kisit8(i,j)$(ord(j)<> ord(i) and s2(i) and s2(j))..u(i)-u(j)+(10*sum(v,x(i,j,v)))=l=9;
-#kisit8(i,j)$(ord(j)<> ord(i) and s2(i) and s2(j))..u(i)-u(j)+(10*sum(v,x(i,j,v)))=l=9;
+# kisit8(i,j)$(ord(j)<> ord(i) and s2(i) and s2(j))..u(i)-u(j)+(10*sum(v,x(i,j,v)))=l=9;
 for i in N:
     for j in NH:
-        m.addConstr(u[j] >= u[i] + 1 + L*(quicksum(x[i][j][v] for v in V)-1 ))
+        m.addConstr(u[j] >= u[i] + 1 + L * (quicksum(x[i][j][v] for v in V) - 1))
 
-#kisit9(i,j,v)$(ord(j)<> ord(i) and s2(j))..sum(k$s1(k),collect(k,i,j,v))=e=x(i,j,v);
-#kisit9(i,j,v)$(ord(j)<> ord(i) and s2(j))..sum(k$s1(k),collect(k,i,j,v))=e=x(i,j,v);
-for i in N:
-    for j in NH:
-        if i != j:
-            for v in V:
-                m.addConstr(quicksum(collect[k, i, j, v] for k in H) == x[i][j][v], name="ctr36_%i%j%v" )
-
-#kisit10(i,j,k,v)  $(s2(j) and s1(k))..collect(k,i,j,v)=l=x(i,j,v);
-# kisit10(i,j,k,v)$(s2(j) and s1(k))..collect(k,i,j,v)=l=x(i,j,v);
-for k in H:
-    for i in N:
-        for j in NH:
-            for v in V:
-                m.addConstr(collect[k, i, j, v] <= x[i][j][v], name="ctr7_%i%j%k%v")
-
-#kisit21(i,j,k,w)$(s2(i) and s1(k) and ord(i)<>ord(j))..distribute(i,j,k,w)=l=x(i,j,w);
-# kisit21(i,j,k,w)$(s2(i) and s1(k) and ord(i)<>ord(j))..distribute(i,j,k,w)=l=x(i,j,w);
-for i in NH:
-    for j in N:
-        if i != j:
-            for k in H:
-                for w in V:
-                    m.addConstr(distribute[i, j, k, w] <= x[i][j][w], name="ctr9_%i%j%k%w")
-
-#kisit24(i,j,w)$(ord(j)<> ord(i) and s2(i))..sum(k$s1(k),distribute(i,j,k,w))=e=x(i,j,w);
-# kisit24(i,j,w)$(ord(j)<> ord(i) and s2(i))..sum(k$s1(k),distribute(i,j,k,w))=e=x(i,j,w);
-for i in NH:
-    for j in N:
-        if i != j:
-            for w in V:
-                m.addConstr(quicksum(distribute[i, j, k, w] for k in H) == x[i][j][w], name="ctr11_%i%j%w")
-
-#kisit5(v)..sum((i,j)$(s1(i) and ord(j)<>ord(i)),x(i,j,v))=e=1;
+# kisit5(v)..sum((i,j)$(s1(i) and ord(j)<>ord(i)),x(i,j,v))=e=1;
 for v in V:
     m.addConstr(
-        quicksum(x[i][j][v] for j in N for i in H if j != i) == 1 )
+        quicksum(x[i][j][v] for j in N for i in H if j != i) == 1)
 
-#kisit80(i)$(s1(i))..sum((v,j)$(ord(j)<>ord(i)),x(i,j,v))=e=(n(i));
+# kisit80(i)$(s1(i))..sum((v,j)$(ord(j)<>ord(i)),x(i,j,v))=e=(n(i));
 for i in H:
     m.addConstr(
-        quicksum(x[i][j][v] for j in N for v in V if j != i) == nv, name="ctr12_%i" )
+        quicksum(x[i][j][v] for j in N for v in V if j != i) == nv, name="ctr12_%i")
 
-#kisit90(i)$(s1(i))..sum((v,j)$(ord(j)<>ord(i)),x(j,i,v))=e=(n(i));
+# kisit90(i)$(s1(i))..sum((v,j)$(ord(j)<>ord(i)),x(j,i,v))=e=(n(i));
 for i in H:
     m.addConstr(
-        quicksum(x[j][i][v] for j in N for v in V if j != i) == nv, name="ctr32_%i" )
+        quicksum(x[j][i][v] for j in N for v in V if j != i) == nv, name="ctr32_%i")
 
-#kisit11(i)$(s2(i))..sum((v,j)$(ord(j)<>ord(i)),x(i,j,v))=e=1;
+# kisit11(i)$(s2(i))..sum((v,j)$(ord(j)<>ord(i)),x(i,j,v))=e=1;
 for i in NH:
     m.addConstr(
-        quicksum(x[i][j][v] for j in N for v in V if j != i) == 1, name="ctr33_%i" )
+        quicksum(x[i][j][v] for j in N for v in V if j != i) == 1, name="ctr33_%i")
 
-#kisit12(i)$(s2(i))..sum((v,j)$(ord(j)<>ord(i)),x(j,i,v))=e=1;
+# kisit12(i)$(s2(i))..sum((v,j)$(ord(j)<>ord(i)),x(j,i,v))=e=1;
 for i in NH:
     m.addConstr(
-        quicksum(x[j][i][v] for j in N for v in V if j != i) == 1, name="ctr34_%i" )
+        quicksum(x[j][i][v] for j in N for v in V if j != i) == 1, name="ctr34_%i")
 
-#kisit23(k,w)$(s1(k))..cost2(k,w)=g= sum((i,j),d(i,j)*distribute(i,j,k,w));
-# kisit23(k,w)$(s1(k))..cost2(k,w)=g= sum((i,j),d(i,j)*distribute(i,j,k,w));
-for k in H:
-    for w in V:
-        m.addConstr(cost2[k, w] >= quicksum(float(D[i][j]) * distribute[i, j, k, w] for i in NH for j in N if i != j), name="ctr15_%k%w")
-
-
-#kisit100(i,k,v,w)$(ord(w)<>ord(v) and ord(i)= ord(k))..cost3=g=cost(i,v)+cost2(k,w);
-# kisit100(i,k,v,w)$(ord(w)<>ord(v) and ord(i)= ord(k))..cost3=g=cost(i,v)+cost2(k,w);
-for k in H:
-    for v in V:
-        for w in V:
-            if v != w:
-                m.addConstr(z >= cost1[k, v] + cost2[k, w], name="ctr16_%k%v%w")
-
-#kisit18(k,v)..cost(k,v)=g= sum((i,j),d(i,j)*collect(k,i,j,v));
-#kisit18(k,v)..cost(k,v)=g= sum((i,j),d(i,j)*collect(k,i,j,v));
-for k in H:
-    for v in V:
-        m.addConstr(cost1[k, v] >= (quicksum(float(D[i][j]) * collect[k, i, j, v] for i in N for j in NH if i != j)), name="ctr17_%k%v" )
-
+# m.addConstr(z == (quicksum(float(D[i][j])*x[i][j][v]) for i in N for j in N if i != j for v in V) ,name="ctr16")
+m.addConstr(
+    z == quicksum(float(D[i][j]) * x[i][j][v] for i in N for j in N for v in V if i != j), name="ctr16")
 
 m.optimize()
 m.write("out.lp")
@@ -200,13 +148,22 @@ m.setObjective(z)
 print('***********************************************************************************')
 links = {}
 route = ""
+# loop to find first moves (H[0], node) in each route and add to links and routes
 for i in range(len(N)):
     for k in range(len(N)):
         for v in V:
-            if (x[i][k][v].x > 0.5):
-                if i != k:
-                    if route == "":
-                        route = str(dictionary.get(N[i])) + "," + str(dictionary.get(N[k]))
+            if x[i][k][v].x > 0.5:
+                if i != k and dictionary.get(N[i]) == dictionary.get(H[0]):
+                    route = str(dictionary.get(N[i])) + "," + str(dictionary.get(N[k]))
+                    links[dictionary.get(N[i])] = dictionary.get(N[k])
+                    print('x(', dictionary.get(N[i]), ',', dictionary.get(N[k]), ',', v, ') ')
+
+# loop on the other edges and add them to links
+for i in range(len(N)):
+    for k in range(len(N)):
+        for v in V:
+            if x[i][k][v].x > 0.5:
+                if i != k and dictionary.get(N[i]) != dictionary.get(H[0]):
                     links[dictionary.get(N[i])] = dictionary.get(N[k])
                     print('x(', dictionary.get(N[i]), ',', dictionary.get(N[k]), ',', v, ') ')
 
@@ -235,8 +192,8 @@ print(route)
 print('***********************************************************************************')
 
 print("dataset instance p nv alpha obj CPU/elapsed Nodes routes")
-print ("Problem:", DATA3.dataset, DATA3.instance, DATA3.p, "alpha:", DATA3.alpha, "Obj:", z.x, "CPU:", CPU,)
-print ('***********************************************************************************')
+print("Problem:", DATA3.dataset, DATA3.instance, DATA3.p, "alpha:", DATA3.alpha, "Obj:", z.x, "CPU:", CPU, )
+print('***********************************************************************************')
 
 import json
 
@@ -245,7 +202,7 @@ with open(DATA3.file_name, 'r+') as f:
     data['CPU'] += CPU
     data['routes'].append(route)
     # data['routes'].sort()
-    f.seek(0)        # <--- should reset fpHC_MTSPile position to the beginning.
+    f.seek(0)  # <--- should reset fpHC_MTSPile position to the beginning.
     json.dump(data, f, indent=4)
-    f.truncate()     # remove remaining part
+    f.truncate()  # remove remaining part
     f.close()
